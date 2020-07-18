6e871e0483f047f22ac865f0a40f88bfc506dfc2
leonevil <leongb66@gmail.com>
2020-07-18 16:27:27 -0300

    Department - findById implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("=== TEST 1: department findById ===");
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		Department department = departmentDao.findById(1);
+		System.out.println(department);
+		
+		
+		sc.close();
+	}
+}
